1def1036d0d412efe69772b141b876601993aacb
Marthyn Olthof <Marthyn@live.nl>
2013-11-18 10:47:56 +0100

    Init Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MedischRekenenArcade::Application.config.secret_key_base = 'f937e7e932224003057b308a82d2577805404a2c68b18d1455e3bf26fed25089622ebc314312fbf5a71b7b30f562ee454cb1141c523ab64349fb1633bb940f76'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MedischRekenenArcade::Application.config.session_store :cookie_store, key: '_MedischRekenenArcade_session'
