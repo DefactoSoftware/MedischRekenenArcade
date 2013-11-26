fb495b43443c27d970c4504006db1634e066f7df
Marthyn Olthof <Marthyn@live.nl>
2013-11-26 11:12:27 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 MedischRekenenArcade::Application.routes.draw do
   devise_for :users
+
+  resources :friendships, only: [:create, :delete]
+
   root to: "home#index"
+
+  resources :users, only: [:show,:index]
+
+  resources :leaderboards, only: [:show, :index]
 end
