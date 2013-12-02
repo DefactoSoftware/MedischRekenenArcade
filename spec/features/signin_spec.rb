474d6f6d2fd6b6ac4cd89b0bfd555b78191333bd
Marthyn Olthof <Marthyn@live.nl>
2013-12-02 11:15:19 +0100

    Fix signin spec with new translations

--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -4,15 +4,14 @@ describe "the user process", type: :feature do
   it "signs me in and out" do
     user = FactoryGirl.create(:user)
     visit "/users/sign_in"
-    expect(page).to have_content "Sign in"
     within("#new_user") do
       fill_in "user_email", with: user.email
       fill_in "user_password", with: "welkom1234"
     end
     click_button "sign_in"
-    expect(page).to have_content "Log out"
+    expect(page).to have_selector('a#menu_logout')
     expect(page).to have_content user.username
-    click_link "Log out"
-    expect(page).to have_content "Log in"
+    click_link "menu_logout"
+    expect(page).to have_selector('a#menu_login')
   end
 end
