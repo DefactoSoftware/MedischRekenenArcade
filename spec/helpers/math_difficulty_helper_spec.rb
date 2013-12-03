32d689b5ee2842b82d8732df5100081b783d1b97
Marthyn Olthof <Marthyn@live.nl>
2013-12-03 13:45:09 +0100

    Implement compute_addition_difficulty

--- spec/helpers/math_difficulty_helper_spec.rb
+++ spec/helpers/math_difficulty_helper_spec.rb
@@ -49,4 +49,34 @@ describe "MathDifficultyHelper" do
       expect(MathDifficultyHelper.compute_subtraction_difficulty([5000007,9])).to eq(22)
     end
   end
+
+  describe "#compute_addition_difficulty" do
+    it "should return 1" do
+      expect(MathDifficultyHelper.compute_addition_difficulty([0,0])).to eq(1)
+    end
+
+    it "should return 1" do
+      expect(MathDifficultyHelper.compute_addition_difficulty([0,1])).to eq(1)
+    end
+
+    it "should return 2" do
+      expect(MathDifficultyHelper.compute_addition_difficulty([1,1])).to eq(2)
+    end
+
+    it "should return 3" do
+      expect(MathDifficultyHelper.compute_addition_difficulty([1,2])).to eq(3)
+    end
+
+    it "should return 2" do
+      expect(MathDifficultyHelper.compute_addition_difficulty([2,2])).to eq(2)
+    end
+
+    it "should return 5" do
+      expect(MathDifficultyHelper.compute_addition_difficulty([19,9])).to eq(5)
+    end
+
+    it "should return 22" do
+      expect(MathDifficultyHelper.compute_addition_difficulty([99999,12345])).to eq(22)
+    end
+  end
 end
