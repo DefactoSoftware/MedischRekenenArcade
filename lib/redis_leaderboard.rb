dff7a127b9fb536d5efd48471ad9026b1aa88039
Marthyn Olthof <Marthyn@live.nl>
2013-12-04 17:00:03 +0100

    Refactor env_db, single line if

--- lib/redis_leaderboard.rb
+++ lib/redis_leaderboard.rb
@@ -1,9 +1,5 @@
 module RedisLeaderboard
   def self.env_db
-    if Rails.env.test?
-      'test_highscores'
-    else
-      'highscores'
-    end
+    Rails.env.test? ? "test_highscores" : "highscores"
   end
 end
