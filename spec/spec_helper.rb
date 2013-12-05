1a396203ae556ca75c33a506561db0539af3dc96
jurre <jurrestender@hotmail.com>
2013-12-05 14:03:38 +0100

    add ruby debugger
    this way there is no need to put debug print statements in the code,
    just put "debugger" in the source wherever you want to break

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'database_cleaner'
 require 'capybara/rspec'
+require 'ruby-debug'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
