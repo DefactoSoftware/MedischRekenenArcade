c04f3d7649915a85fe61cc15b2bc6ad97e3e373d
Marthyn Olthof <Marthyn@live.nl>
2013-12-09 16:12:15 +0100

    Pass default options to RedisLeaderboard

--- lib/redis_leaderboard.rb
+++ lib/redis_leaderboard.rb
@@ -3,6 +3,6 @@ module RedisLeaderboard
     Rails.env.test? ? "test_highscores" : "highscores"
   end
   def self.get
-    Leaderboard.new(env_db, {redis_connection: $redis})
+    Leaderboard.new(env_db, Leaderboard::DEFAULT_OPTIONS, {redis_connection: $redis})
   end
 end
