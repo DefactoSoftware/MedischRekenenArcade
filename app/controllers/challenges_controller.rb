1d7e5cef8c96c5768e7e659840fa465713c7b30c
Marthyn Olthof <Marthyn@live.nl>
2013-12-10 14:15:09 +0100

    Add show method for challengescontroller, work with session vars to store current challenge information

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,7 +1,13 @@
 class ChallengesController < ApplicationController
   def show
     @challenge = Challenge.where(name: params[:id]).first
-    @problem = Problem.generate_random_with_skills(1, @challenge.skills)
+    if !session[:challenge]
+      session[:challenge] = @challenge.id
+      session[:start] = DateTime.now
+      UserChallenge.create(challenge: @challenge, user:current_user)
+      flash[:notice] = "Starting new challenge, good luck! If you make more than 6 mistakes the patient dies so be careful!"
+    end
+    @problem = Problem.generate_random_with_skills(3, @challenge.skills)
   end
 
   def index
