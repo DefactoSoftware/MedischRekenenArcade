2bb9b9a10cc20209f5a85882e5d14e5f39575a27
Marthyn Olthof <Marthyn@live.nl>
2013-12-10 15:04:10 +0100

    Remove deprecation message

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module MedischRekenenArcade
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    #config.i18n.default_locale = :nl
+    I18n.enforce_available_locales = false
   end
 end
