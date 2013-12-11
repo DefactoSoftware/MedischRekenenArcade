82b7654b1b9fc46e31eb6e0cfe08210bb1e2b43d
Marthyn Olthof <Marthyn@live.nl>
2013-12-11 11:31:25 +0100

    Reset challenge when not on challenge page anymore and add better messages(localized)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_filter :update_sanitized_params, if: :devise_controller?
+  before_filter :check_challenge
   protect_from_forgery with: :exception
 
   protected
@@ -9,5 +10,17 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:email, :username, :name, :password, :password_confirmation, :profile_picture_url, :user_group_id)}
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update)  {|u| u.permit(:email, :username, :name, :password, :password_confirmation, :current_password, :profile_picture_url, :user_group_id)}
+  end
+
+  def check_challenge
+    if params[:controller] != "challenges" && params[:controller] != "answers"
+      reset_challenge
+    end
+  end
+
+  def reset_challenge
+    session.delete(:damage)
+    session.delete(:start)
+    session.delete(:challenge)
   end
 end
