08b76c864d204a49f67357772cd84183c1b804f2
Marthyn Olthof <Marthyn@live.nl>
2013-12-12 10:39:51 +0100

    Add migration for user_challenge reference in Answer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131211113118) do
+ActiveRecord::Schema.define(version: 20131212092307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,10 @@ ActiveRecord::Schema.define(version: 20131211113118) do
     t.integer  "problem_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_challenge_id"
   end
+
+  add_index "answers", ["user_challenge_id"], name: "index_answers_on_user_challenge_id", using: :btree
 
   create_table "challenge_skills", force: true do |t|
     t.integer  "skill_id"
