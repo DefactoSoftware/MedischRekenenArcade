aa0b5622cf31d61ae125fe79e78e2b08cb33f4d1
Marthyn Olthof <Marthyn@live.nl>
2013-12-12 10:40:09 +0100

    Create tests for refactoring answerscontroller

--- spec/controllers/challenges_controller_spec.rb
+++ spec/controllers/challenges_controller_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe ChallengesController do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:challenge) { FactoryGirl.create(:challenge) }
+  describe "GET index" do
+    before :each do
+      sign_in user.reload
+    end
+    it "should render template" do
+      get :index
+      expect(response.status).to eq(200)
+    end
 
+    it "should assign @challenges" do
+      get :index
+      expect(assigns(:challenges)).to eq(Challenge.all)
+    end
+  end
+
+  describe "GET show" do
+    before :each do
+      sign_in user.reload
+    end
+
+    it "should render template" do
+      get :show
+    end
+  end
 end
