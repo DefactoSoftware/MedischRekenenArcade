e31720efe4faf6ebf49e1ac0496dd8dba31be65b
Marthyn Olthof <Marthyn@live.nl>
2013-12-16 16:56:47 +0100

    Add GameSession to wrap the Ruby session

--- lib/answer_handler.rb
+++ lib/answer_handler.rb
@@ -1,3 +1,50 @@
+class GameSession
+  def initialize(session)
+    @session = session
+  end
+
+  def streak
+    @session[:streak]
+  end
+
+  def damage
+    @session[:damage]
+  end
+
+  def start_time
+    @session[:start_time]
+  end
+
+  def challenge_id
+    @session[:challenge_id]
+  end
+
+  def reset_challenge!
+    @session.delete(:damage)
+    @session.delete(:start)
+    @session.delete(:challenge)
+  end
+
+  def reset_streak!
+    streak = 0
+    @session[:streak] = 0
+  end
+
+  def decrease_damage!
+    if @session[:damage] && @session[:damage] > 0
+      @session[:damage] = @session[:damage].to_i - 1
+    end
+  end
+
+  def increase_damage!
+    @session[:damage] = @session[:damage].to_i + 1
+  end
+
+  def increase_streak!(value=1)
+    @session[:streak] =  @session[:streak].to_i + value
+  end
+end
+
 class AnswerHandler
   attr_reader :session, :user, :answer_is_correct
 
@@ -6,45 +53,23 @@ class AnswerHandler
 
   def initialize(session, current_user)
     @user = current_user
-    @session = session
+    @session = GameSession.new(session)
   end
 
-  def reset_challenge
-    session.delete(:damage)
-    session.delete(:start)
-    session.delete(:challenge)
+  def method_missing(method, *args, &block)
+    session.send(method, *args, &block)
   end
 
-  def reset_streak
-    session[:streak] = 0
-  end
-
-  def decrease_damage
-    if session[:damage] && session[:damage] > 0
-      session[:damage] = session[:damage].to_i - 1
-    end
-  end
-
-  def increase_damage
-    session[:damage] = session[:damage].to_i + 1
-  end
-
-  def increase_streak(value=1)
-    session[:streak] =  session[:streak].to_i + value
-  end
-
-  def increase_points(value=1)
+  def increase_points!(value=1)
     user.increase_points(value)
   end
 
-  def decrease_points(value=1)
+  def decrease_points!(value=1)
     user.decrease_points(value)
   end
 end
 
 class ChallengeAnswerHandler < AnswerHandler
-  include Rails.application.routes.url_helpers
-
   attr_reader :challenge, :user_challenge
 
   STANDARD_DEATH_CEILING = 6
@@ -59,20 +84,22 @@ class ChallengeAnswerHandler < AnswerHandler
     @finished ||= user_challenge.amount_good + 1  >= challenge.number_of_problems
   end
 
-  def update_user_challenge
+  def update_user_challenge!
     if is_dead || finished
-      reset_challenge
+      reset_challenge!
     end
   end
 
   def redirect_path
     if is_dead || finished
       Rails.application.routes.url_helpers.challenges_path
+    else
+      Rails.application.routes.url_helpers.challenge_path(challenge)
     end
   end
 
   def is_dead
-    session[:damage] && session[:damage] > STANDARD_DEATH_CEILING
+    session.damage && session.damage > STANDARD_DEATH_CEILING
   end
 end
 
@@ -81,14 +108,15 @@ class CorrectChallengeAnswerHandler < ChallengeAnswerHandler
     super(session, current_user, user_challenge)
   end
 
-  def handle
-    increase_points(challenge.bonus) if finished
-    increase_points(AnswerHandler::STANDARD_POINT_AMOUNT)
-    increase_streak(AnswerHandler::STANDARD_STREAK_AMOUNT)
-    decrease_damage
+  def handle!
+    increase_points!(challenge.bonus) if finished
+    increase_points!(AnswerHandler::STANDARD_POINT_AMOUNT)
+    increase_streak!(AnswerHandler::STANDARD_STREAK_AMOUNT)
+    decrease_damage!
+    update_user_challenge!
   end
 
-  def update_user_challenge
+  def update_user_challenge!
     super
     user_challenge.update_attributes(amount_good: user_challenge.amount_good + 1)
   end
@@ -106,13 +134,13 @@ class IncorrectChallengeAnswerHandler < ChallengeAnswerHandler
     super(session, current_user, user_challenge)
   end
 
-  def handle
-    increase_damage
-    reset_streak
-    update_user_challenge
+  def handle!
+    increase_damage!
+    reset_streak!
+    update_user_challenge!
   end
 
-  def update_user_challenge
+  def update_user_challenge!
     super
     user_challenge.update_attributes(success: false) if is_dead
     user_challenge.update_attributes(amount_fail: user_challenge.amount_fail + 1)
@@ -139,9 +167,9 @@ class CorrectPracticeAnswerHandler < PracticeAnswerHandler
     super(session, current_user)
   end
 
-  def handle
-    increase_points AnswerHandler::STANDARD_POINT_AMOUNT
-    increase_streak AnswerHandler::STANDARD_STREAK_AMOUNT
+  def handle!
+    increase_points! AnswerHandler::STANDARD_POINT_AMOUNT
+    increase_streak! AnswerHandler::STANDARD_STREAK_AMOUNT
   end
 
   def get_notice
@@ -154,8 +182,8 @@ class IncorrectPracticeAnswerHandler < PracticeAnswerHandler
     super(session, current_user)
   end
 
-  def handle
-    reset_streak
+  def handle!
+    reset_streak!
   end
 
   def get_notice
@@ -164,5 +192,30 @@ class IncorrectPracticeAnswerHandler < PracticeAnswerHandler
 end
 
 class AnswerHandlerFactory
+  def initialize(session, answer_is_correct, user)
+    @session = session
+    @answer_is_correct = answer_is_correct
+    @user = user
+  end
 
+  def build
+    if @session[:challenge]
+      if @answer_is_correct
+        CorrectChallengeAnswerHandler.new(@session, @user, user_challenge)
+      else
+        IncorrectChallengeAnswerHandler.new(@session, @user, user_challenge)
+      end
+    else
+      if @answer_is_correct
+        CorrectPracticeAnswerHandler.new(@session, @user)
+      else
+        IncorrectPracticeAnswerHandler.new(@session, @user)
+      end
+    end
+  end
+
+  private
+  def user_challenge
+    UserChallenge.where(challenge: Challenge.find(@session[:challenge]), user: @user).last
+  end
 end
--- spec/lib/answer_handler_spec.rb
+++ spec/lib/answer_handler_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-require 'support/devise'
 
 describe AnswerHandler do
   include Rails.application.routes.url_helpers
@@ -9,9 +8,9 @@ describe AnswerHandler do
   let(:user_challenge) { UserChallenge.new(challenge:challenge, user:user) }
   let(:practicehandler_good) { CorrectPracticeAnswerHandler.new({}, user) }
   let(:practicehandler_bad) { IncorrectPracticeAnswerHandler.new({}, user) }
-  let(:challenge_handler_good) { CorrectChallengeAnswerHandler.new({ challenge: challenge.id}, user, user_challenge)}
-  let(:challenge_handler_dead) { IncorrectChallengeAnswerHandler.new({ challenge: challenge.id, damage: ChallengeAnswerHandler::STANDARD_DEATH_CEILING + 1 }, user, user_challenge) }
-  let(:challenge_handler_wrong) { IncorrectChallengeAnswerHandler.new({ challenge: challenge.id}, user, user_challenge) }
+  let(:challenge_handler_good) { CorrectChallengeAnswerHandler.new({ challenge_id: challenge.id}, user, user_challenge)}
+  let(:challenge_handler_dead) { IncorrectChallengeAnswerHandler.new({ challenge_id: challenge.id, damage: ChallengeAnswerHandler::STANDARD_DEATH_CEILING + 1 }, user, user_challenge) }
+  let(:challenge_handler_wrong) { IncorrectChallengeAnswerHandler.new({ challenge_id: challenge.id}, user, user_challenge) }
 
 
 
@@ -21,7 +20,7 @@ describe AnswerHandler do
 
     describe "deletes the session" do
       before(:each) do
-        handler.reset_challenge
+        handler.reset_challenge!
       end
 
       it "damage" do
@@ -38,33 +37,33 @@ describe AnswerHandler do
     end
   end
 
-  describe "#decrease_damage" do
+  describe "#reset_streak!" do
     let(:handler) { AnswerHandler.new({streak: 1}, double)}
     it "should reset the sessions streak" do
-      handler.reset_streak
-      expect(handler.session[:streak]).to eq(0)
+      handler.reset_streak!
+      expect(handler.session.streak).to eq(0)
     end
   end
 
   describe "#decrease_damage" do
     let(:handler) { AnswerHandler.new({ damage: 2 }, double)}
     it "should decrease to 1 when damage is 2" do
-      handler.decrease_damage
-      expect(handler.session[:damage]).to eq(1)
+      handler.decrease_damage!
+      expect(handler.session.damage).to eq(1)
     end
 
     let(:handler2) { AnswerHandler.new({damage:1}, double)}
     it "should return 0 when damage is 1" do
-      handler2.decrease_damage
-      expect(handler2.session[:damage]).to eq(0)
+      handler2.decrease_damage!
+      expect(handler2.session.damage).to eq(0)
     end
   end
 
   describe "#increase_streak" do
     let(:handler) { AnswerHandler.new({streak:0}, user) }
     it "should increase the streak" do
-      handler.increase_streak
-      expect(handler.session[:streak]).to be(1)
+      handler.increase_streak!
+      expect(handler.session.streak).to eq(1)
     end
   end
 
@@ -73,7 +72,7 @@ describe AnswerHandler do
     let(:handler) { AnswerHandler.new(double, mock_user) }
     it "should increase the points" do
       expect(mock_user).to receive(:increase_points).with(1)
-      handler.increase_points
+      handler.increase_points!
     end
   end
 
@@ -82,7 +81,7 @@ describe AnswerHandler do
       let(:handler) { AnswerHandler.new(double, mock_user) }
       it "should decrease the points" do
         expect(mock_user).to receive(:decrease_points).with(1)
-        handler.decrease_points
+        handler.decrease_points!
       end
     end
   end
@@ -146,6 +145,139 @@ describe AnswerHandler do
 end
 
 describe AnswerHandlerFactory do
+  let(:session) { double }
+  let(:answer_is_correct) { double }
+  let(:user) { double }
+  let(:factory) { AnswerHandlerFactory.new(session, answer_is_correct, user) }
 
+  describe "#initialize" do
+    it "should initialize session" do
+      expect(factory.instance_variable_get(:@session)).to eq(session)
+    end
+
+    it "should intialize answer_is_correct" do
+      expect(factory.instance_variable_get(:@answer_is_correct)).to eq(answer_is_correct)
+    end
+
+    it "should initialize user" do
+      expect(factory.instance_variable_get(:@user)).to eq(user)
+    end
+  end
+
+  describe "#build" do
+    describe "when challenge" do
+      let(:session) { { challenge: 1 } }
+
+      before(:each) do
+        expect(factory).to receive(:user_challenge).and_return(double(challenge: double))
+      end
+
+      describe "when answer is incorrect" do
+        let(:answer_is_correct) { false }
+
+        describe "when dead" do
+          let(:session) { {damage: 6, challenge: 1} }
+          it "should return an IncorrectChallengeAnswerHandler" do
+            expect(factory.build).to be_instance_of(IncorrectChallengeAnswerHandler)
+          end
+        end
+
+        describe "when not dead" do
+          let(:session) { { damage: rand(0...5), challenge: 1 } }
+          it "should return an IncorrectChallengeAnswerHandler" do
+            expect(factory.build).to be_instance_of(IncorrectChallengeAnswerHandler)
+          end
+        end
+      end
+
+      describe "when answer is correct" do
+        let(:session) { { challenge: 1 } }
+        let(:answer_is_correct) { true }
+
+        describe "when finished" do
+          it "should return a CorrectChallengeAnswerHandler" do
+            expect(factory.build).to be_instance_of(CorrectChallengeAnswerHandler)
+          end
+        end
+
+        describe "when not finished" do
+          it "should return a CorrectChallengeAnswerHandler" do
+            expect(factory.build).to be_instance_of(CorrectChallengeAnswerHandler)
+          end
+        end
+      end
+    end
+
+    describe "when practice" do
+      let(:session) { { challenge: nil } }
+
+      describe "when answer is incorrect" do
+        let(:answer_is_correct) { false }
+
+        it "should return an IncorrectPracticeAnswerHandler" do
+          expect(factory.build).to be_instance_of(IncorrectPracticeAnswerHandler)
+        end
+      end
+
+      describe "when answer is correct" do
+        let(:answer_is_correct) { true }
+
+        it "should return a CorrectPracticeAnswerHandler" do
+          expect(factory.build).to be_instance_of(CorrectPracticeAnswerHandler)
+        end
+      end
+    end
+  end
 end
 
+describe GameSession do
+  let(:game_session) { GameSession.new(session) }
+  describe "#initialize" do
+    describe "when challenge" do
+      let(:session) { { challenge_id: double, start_time: double, damage: double, streak: double } }
+
+      it "initializes damage" do
+        expect(game_session.damage).to_not eq(nil)
+      end
+
+      it "initializes start_time" do
+        expect(game_session.start_time).to_not eq(nil)
+      end
+
+      it "initializes challenge_id" do
+        expect(game_session.challenge_id).to_not eq(nil)
+      end
+
+      it "initializes session" do
+        expect(game_session.instance_variable_get(:@session)).to_not eq(nil)
+      end
+
+      it "initalizes streak" do
+        expect(game_session.streak).to_not eq(nil)
+      end
+    end
+
+    describe "when practice" do
+      let(:session) { { streak: double } }
+      it "does not initialize damage" do
+        expect(game_session.damage).to eq(nil)
+      end
+
+      it "does not initialize start_time" do
+        expect(game_session.start_time).to eq(nil)
+      end
+
+      it "does not initialize challenge_id" do
+        expect(game_session.challenge_id).to eq(nil)
+      end
+
+      it "initializes session" do
+        expect(game_session.instance_variable_get(:@session)).to_not eq(nil)
+      end
+
+      it "initalizes streak" do
+        expect(game_session.streak).to_not eq(nil)
+      end
+    end
+  end
+end
