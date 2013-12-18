011145195e4f0c8b7297be45e85b4261a15eaf9f
Marthyn Olthof <Marthyn@live.nl>
2013-12-18 14:06:00 +0100

    add merit to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,8 @@
 #
 
 class User < ActiveRecord::Base
+  has_merit
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
