6ad889501ab3b7e335d54c830c14daed829e8e72
Marthyn Olthof <Marthyn@live.nl>
2013-12-18 12:49:27 +0100

    Add popover to challenge overview

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -16,4 +16,23 @@ class Challenge < ActiveRecord::Base
   has_many :challenge_skills
   has_many :skills, through: :challenge_skills
   has_many :user_challenges
+
+
+  def total_correct_answers_for_user(user)
+    userchallenges = self.user_challenges.where(user: user)
+    userchallenges.reduce(0) { |total, userchallenge|  total += userchallenge.amount_good }
+  end
+
+  def total_incorrect_answers_for_user(user)
+    userchallenges = self.user_challenges.where(user: user)
+    userchallenges.reduce(0) { |total, userchallenge|  total += userchallenge.amount_fail }
+  end
+
+  def total_success_for_user(user)
+    self.user_challenges.where(user: user, success: true).count
+  end
+
+  def total_failed_for_user(user)
+    self.user_challenges.where(user: user, success: false || nil).count
+  end
 end
