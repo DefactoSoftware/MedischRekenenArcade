238b3dd10aa975f32eb2e215435d1d27d79d8d96
Marthyn Olthof <Marthyn@live.nl>
2013-12-20 10:30:55 +0100

    Restore code lost in rebase

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:account_update)  {|u| u.permit(:email, :username, :name, :password, :password_confirmation, :current_password, :profile_picture_url, :user_group_id)}
   end
 
+  def check_challenge
+    if params[:controller] != "challenges" && params[:controller] != "answers"
+      reset_challenge
+    end
+  end
+
   def reset_challenge
     session.delete(:damage)
     session.delete(:start)
