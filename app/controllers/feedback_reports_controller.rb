24ce11f545bfe79595496200a648f761dd980bbd
Marthyn Olthof <Marthyn@live.nl>
2014-01-20 12:39:39 +0100

    Add more specs for feedback

--- app/controllers/feedback_reports_controller.rb
+++ app/controllers/feedback_reports_controller.rb
@@ -2,6 +2,7 @@ class FeedbackReportsController < ApplicationController
   def create
     @feedback_report = FeedbackReport.new(feedback_report_parameters)
     @feedback_report.user = current_user
+    @feedback_report.page = request.referer
 
     if @feedback_report.save!
       redirect_to request.referer, notice: t("feedback_report.save.success")
--- spec/controllers/feedback_reports_controller_spec.rb
+++ spec/controllers/feedback_reports_controller_spec.rb
@@ -5,11 +5,22 @@ describe FeedbackReportsController do
     let(:user) { FactoryGirl.create(:user) }
     let(:feedback_report_attributes) { FactoryGirl.attributes_for(:feedback_report) }
 
-    it "creates a report" do
+    before :each do
       @request.env['HTTP_REFERER'] = root_path
       sign_in user
       post :create, feedback_report: feedback_report_attributes
+    end
+
+    it "creates a report with text" do
       expect(FeedbackReport.last.text).to eq(feedback_report_attributes[:text])
+    end
+
+    it "creates a report with subject" do
+      expect(FeedbackReport.last.subject).to eq(feedback_report_attributes[:subject])
+    end
+
+    it "creates a report with page equal to request referer" do
+      expect(FeedbackReport.last.page).to eq(root_path)
     end
   end
 end
