880d617691c07061680fbd0188772c4a66fea227
Marthyn Olthof <Marthyn@live.nl>
2014-01-30 13:56:18 +0100

    Fix photo upload, Fix #98

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,9 @@ class ApplicationController < ActionController::Base
 
   protected
   def update_sanitized_params
-    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:email, :username, :name, :password, :password_confirmation, :profile_picture_url, :user_group_id)}
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:email, :username, :name, :password, :password_confirmation, :profilepicture_url, :user_group_id)}
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update)  {|u| u.permit(:email, :username, :name, :password, :password_confirmation, :current_password, :profile_picture_url, :user_group_id)}
+    devise_parameter_sanitizer.for(:account_update)  {|u| u.permit(:email, :username, :name, :password, :password_confirmation, :current_password, :profilepicture_url, :user_group_id)}
   end
 
   def check_challenge
