f0d4ae269a621220bbdbdb97f98dd5c7c8b7ef17
Marthyn Olthof <Marthyn@live.nl>
2014-01-30 13:16:04 +0100

    Snake case variables

--- spec/lib/difficulty_calculator_spec.rb
+++ spec/lib/difficulty_calculator_spec.rb
@@ -17,37 +17,37 @@ describe FormulaDifficultyCalculator do
 
   describe "#compute_subtraction_difficulty" do
     it "20 - 10 is easier than 64 - 19" do
-      difficultyA = SubtractionOperationDifficultyCalculator.new([2,5], [1,5]).compute_difficulty
-      difficultyB = SubtractionOperationDifficultyCalculator.new([6,4], [1,8]).compute_difficulty
+      difficulty_a = SubtractionOperationDifficultyCalculator.new([2,5], [1,5]).compute_difficulty
+      difficulty_b = SubtractionOperationDifficultyCalculator.new([6,4], [1,8]).compute_difficulty
 
-      expect(difficultyA).to be < difficultyB
+      expect(difficulty_a).to be < difficulty_b
     end
   end
 
   describe "#compute_addition_difficulty" do
     it "5 + 15 is easier than 13 + 29" do
-      difficultyA = AdditionOperationDifficultyCalculator.new([5], [1,5]).compute_difficulty
-      difficultyB = AdditionOperationDifficultyCalculator.new([1,3], [2,9]).compute_difficulty
+      difficulty_a = AdditionOperationDifficultyCalculator.new([5], [1,5]).compute_difficulty
+      difficulty_b = AdditionOperationDifficultyCalculator.new([1,3], [2,9]).compute_difficulty
 
-      expect(difficultyA).to be < difficultyB
+      expect(difficulty_a).to be < difficulty_b
     end
   end
 
   describe "#compute_multiplication_difficulty" do
     it "10 * 1 is easier than 23 * 11" do
-      difficultyA = MultiplicationOperationDifficultyCalculator.new([1,0],[1]).compute_difficulty
-      difficultyB = MultiplicationOperationDifficultyCalculator.new([2,3],[1,1]).compute_difficulty
+      difficulty_a = MultiplicationOperationDifficultyCalculator.new([1,0],[1]).compute_difficulty
+      difficulty_b = MultiplicationOperationDifficultyCalculator.new([2,3],[1,1]).compute_difficulty
 
-      expect(difficultyA).to be < difficultyB
+      expect(difficulty_a).to be < difficulty_b
     end
   end
 
   describe "#compute_division" do
     it "10 / 10 is easier than 19 / 3" do
-      difficultyA = DivisionOperationDifficultyCalculator.new([1,0], [1,0]).compute_difficulty
-      difficultyB = DivisionOperationDifficultyCalculator.new([1,9], [3]).compute_difficulty
+      difficulty_a = DivisionOperationDifficultyCalculator.new([1,0], [1,0]).compute_difficulty
+      difficulty_b = DivisionOperationDifficultyCalculator.new([1,9], [3]).compute_difficulty
 
-      expect(difficultyA).to be < difficultyB
+      expect(difficulty_a).to be < difficulty_b
     end
   end
 end
