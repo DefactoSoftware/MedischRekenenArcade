40c1dc8068be6cf432a2962f5652dd9640a9b5a9
Marthyn Olthof <Marthyn@live.nl>
2014-02-17 14:45:54 +0100

    Add serializer for problem

--- app/controllers/api/v1/problems_controller.rb
+++ app/controllers/api/v1/problems_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class ProblemsController < BaseController
       def index
-        render json: ProblemFactory.new(params[:problem] || "Mixed", current_user).problem, status: :created
+        respond_with ProblemSerializer.new(ProblemFactory.new(params[:problem] || "Mixed", current_user).problem).to_json, status: :created
       end
     end
   end
