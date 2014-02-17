e254e6830a9cd89f84ddd2627b12c81377fc69da
Marthyn Olthof <Marthyn@live.nl>
2014-02-17 13:28:24 +0100

    Add api basecontroller

---
+++ app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,16 @@
+module Api
+  module V1
+    class BaseController < ApplicationController
+      skip_before_filter :verify_authenticity_token
+      doorkeeper_for :all
+      respond_to :json
+
+      private
+      def current_user
+        if doorkeeper_token
+          @current_user ||= User.find(doorkeeper_token.resource_owner_id)
+        end
+      end
+    end
+  end
+end
