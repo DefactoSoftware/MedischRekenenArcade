7e5f25963bc66c1356ad32f70be3c60de19e41f9
Marthyn Olthof <Marthyn@live.nl>
2014-02-20 15:06:46 +0100

    Fix top leaderboard issue

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -3,7 +3,7 @@ class LeaderboardsController < ApplicationController
   before_action :authenticate_user!
   def index
     highscore_lb = RedisLeaderboard.get
-    @top_scores = highscore_lb.leaders(10)
+    @top_scores = highscore_lb.leaders(1)
     @user_group_scores = highscore_lb.ranked_in_list(user_group_users, sort_by: :rank)
     @friend_scores = highscore_lb.ranked_in_list(friend_users, sort_by: :rank)
   end
