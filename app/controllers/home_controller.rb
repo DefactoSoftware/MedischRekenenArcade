632fedf0d68750b9d61ea957facdabc55dea989d
Marthyn Olthof <Marthyn@live.nl>
2014-02-21 11:50:31 +0100

    Decrease page size home leaderboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     if current_user
       highscore_lb = RedisLeaderboard.get
-      @scores = highscore_lb.ranked_in_list(user_group_users, sort_by: :rank)
+      @scores = highscore_lb.ranked_in_list(user_group_users, sort_by: :rank, page_size: 1)
       challenges = UserChallenge.where(user: current_user)
       @last_challenge = challenges.last.challenge if !challenges.empty?
       @last_badge = current_user.badges.last
