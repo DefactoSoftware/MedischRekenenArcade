8e561d4ec003132f54b4dc1640763f10003525c3
Marthyn Olthof <Marthyn@live.nl>
2014-03-04 15:47:37 +0100

    Fix size leaderboard home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     if current_user
       highscore_lb = RedisLeaderboard.get
-      @scores = highscore_lb.ranked_in_list(user_group_users, sort_by: :rank, page_size: 1)
+      @scores = highscore_lb.ranked_in_list(user_group_users, sort_by: :rank).first(10)
       challenges = UserChallenge.where(user: current_user)
       @last_challenge = challenges.last.challenge if !challenges.empty?
       @last_badge = current_user.badges.last
