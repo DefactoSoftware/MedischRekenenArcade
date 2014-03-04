10c178d4ada8dd623dec505cd244438877bfc133
Marthyn Olthof <Marthyn@live.nl>
2014-03-04 16:02:32 +0100

    Change leaderboards

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -5,15 +5,11 @@ class LeaderboardsController < ApplicationController
     highscore_lb = RedisLeaderboard.get
     @top_scores = highscore_lb.leaders(1)
     @user_group_scores = highscore_lb.ranked_in_list(user_group_users, sort_by: :rank)
-    @friend_scores = highscore_lb.ranked_in_list(friend_users, sort_by: :rank)
+    @around_me_scores = highscore_lb.around_me(current_user.id, sort_by: :rank)
   end
 
   private
   def user_group_users
     User.where(user_group: current_user.user_group).map(&:id)
-  end
-
-  def friend_users
-    current_user.friends.map(&:id)
   end
 end
