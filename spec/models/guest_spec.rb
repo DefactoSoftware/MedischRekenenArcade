6e62fe96fb9e7c9441f33614046076b613f1621f
Marthyn Olthof <Marthyn@live.nl>
2014-03-05 13:48:13 +0100

    Scaffold guest spec

---
+++ spec/models/guest_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Guest do
+  it "has a name method" do
+
+  end
+
+  it "has a username method" do
+
+  end
+
+  it "has an answers method" do
+
+  end
+
+  it "has a user group method" do
+
+  end
+
+  it "has an activities method" do
+
+  end
+
+  it "has a recent activities method" do
+
+  end
+
+  it "has a friends with method" do
+
+  end
+end
