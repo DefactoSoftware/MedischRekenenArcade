9aec1d2d7dbc337c419b9b7eadb03880a928e976
Marthyn Olthof <Marthyn@live.nl>
2014-03-10 14:34:12 +0100

    Update guest model

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -10,4 +10,20 @@ class Guest
   def name
     "guest"
   end
+
+  def user_challenges
+    UserChallenge.none
+  end
+
+  def badges
+    Merit::BadgesSash.none
+  end
+
+  def answers
+    Answer.none
+  end
+
+  def user_skills
+    UserSkill.none
+  end
 end
