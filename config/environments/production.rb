e63fe02cc748de994e06575810c9e4cca10aa7b0
Marthyn Olthof <Marthyn@live.nl>
2014-03-26 12:58:34 +0100

    Change host to lerendoseren.nl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 MedischRekenenArcade::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { :host => 'medischrekenen.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'lerendoseren.nl' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
