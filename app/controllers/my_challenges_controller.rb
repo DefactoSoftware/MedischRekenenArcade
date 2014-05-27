aad3186cbf842f81bf663b8f795525999f1f4ce4
Marthyn Olthof <Marthyn@live.nl>
2014-05-27 10:05:46 +0200

    Add head to head to my challenges

--- app/controllers/my_challenges_controller.rb
+++ app/controllers/my_challenges_controller.rb
@@ -1,8 +1,10 @@
 class MyChallengesController < ApplicationController
   before_action :authenticate_user!
   def index
+    @challenge_sets = ChallengeSet.where(locked:false).order("NAME ASC")
     @head_to_head_challenges = current_user.head_to_head_challenges
     @normal_challenges = current_user.normal_challenges
+    @user = current_user
   end
 
   def show
