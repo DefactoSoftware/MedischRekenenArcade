0fb01c5c04c52f17415d10645b32b2ba2073a27a
Marthyn Olthof <Marthyn@live.nl>
2014-05-27 14:20:41 +0200

    Style my challenges

--- app/controllers/my_challenges_controller.rb
+++ app/controllers/my_challenges_controller.rb
@@ -1,7 +1,7 @@
 class MyChallengesController < ApplicationController
   before_action :authenticate_user!
   def index
-    @challenge_sets = ChallengeSet.where(locked:false).order("NAME ASC")
+    @challenge_sets = ChallengeSet.where(locked: false).order("NAME ASC")
     @head_to_head_challenges = current_user.head_to_head_challenges
     @normal_challenges = current_user.normal_challenges
     @user = current_user
