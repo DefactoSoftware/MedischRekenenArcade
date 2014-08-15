45885d9c84f31c61857d7cd69cf38fa79530a892
Marthyn Olthof <Marthyn@live.nl>
2014-08-15 11:53:12 +0200

    add users controller spec

--- spec/lib/answer_handler_spec.rb
+++ spec/lib/answer_handler_spec.rb
@@ -206,6 +206,9 @@ describe AnswerHandler do
       expect(head_to_head_challenge_handler_good.get_notice).to eq(
         I18n.t("head_to_head_challenge.finished")
       )
+      head_to_head_challenge_handler_good.handle!
+      expect(head_to_head_challenge_handler_good).to receive(:finish)
+      expect(head_to_head_challenge_handler_good).to receive(:challenge_set_completion_badge)
     end
 
     it "returns answer is correct as notice when answer is wrong" do
@@ -385,65 +388,6 @@ describe AnswerHandlerFactory do
           expect(factory.build)
           .to be_instance_of(CorrectGuestAnswerHandler)
         end
-      end
-    end
-  end
-end
-
-describe GameSession do
-  let(:game_session) { GameSession.new(session) }
-  describe "#initialize" do
-    describe "when challenge" do
-      let(:session) do
-        {
-          challenge_id: double,
-          start_time: double,
-          damage: double,
-          streak: double
-        }
-      end
-
-      it "initializes damage" do
-        expect(game_session.damage).to_not eq(nil)
-      end
-
-      it "initializes start_time" do
-        expect(game_session.start_time).to_not eq(nil)
-      end
-
-      it "initializes challenge_id" do
-        expect(game_session.challenge_id).to_not eq(nil)
-      end
-
-      it "initializes session" do
-        expect(game_session.instance_variable_get(:@session)).to_not eq(nil)
-      end
-
-      it "initalizes streak" do
-        expect(game_session.streak).to_not eq(nil)
-      end
-    end
-
-    describe "when practice" do
-      let(:session) { { streak: double } }
-      it "does not initialize damage" do
-        expect(game_session.damage).to eq(nil)
-      end
-
-      it "does not initialize start_time" do
-        expect(game_session.start_time).to eq(nil)
-      end
-
-      it "does not initialize challenge_id" do
-        expect(game_session.challenge_id).to eq(nil)
-      end
-
-      it "initializes session" do
-        expect(game_session.instance_variable_get(:@session)).to_not eq(nil)
-      end
-
-      it "initalizes streak" do
-        expect(game_session.streak).to_not eq(nil)
       end
     end
   end
